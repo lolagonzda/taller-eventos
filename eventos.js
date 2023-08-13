50937952d996de91f2200076ec52b2ded2729202
lolagonzda <lorenagonzda@gmail.com>
2023-08-13 20:14:11 -0300

    arreglo

--- eventos.js
+++ eventos.js
@@ -1,13 +1,15 @@
-const container = document.getElementById("container");
-const btn = document.querySelector ("button")
-
-
-function saludar () {
-    alert ("Hola! Soy el div")
+function funcionSaludo(){
+    alert ("Hola!");
 }
+let btn = document.getElementById("btn");
+let miDiv = document.getElementById('miDiv');
 
-container.addEventListener("click", saludar);
+miDiv.addEventListener("click", function() {
+    alert("Hola! Soy el div");
+});
 
-btn.addEventListener("click", function(saludar) {
-    saludar.stopPropagation();
+/*Lo que hago con este método es evitar que el alert del div
+se dispare dos veces*/
+btn.addEventListener("click", function(event) {
+    event.stopPropagation(); 
 });
